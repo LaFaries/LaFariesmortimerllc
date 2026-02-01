fbe46de672e81b2c0d8ce8c38a1ce16740e9abeb
LaFaries <lafaries1225@gmail.com>
2026-02-01 15:45:00 -0500

    Update generate-contract.js

--- netlify/functions/generate-contract.js
+++ netlify/functions/generate-contract.js
@@ -3,17 +3,10 @@ const path = require("path");
 
 exports.handler = async (event) => {
   try {
-    const { client_name, service_fee } = JSON.parse(event.body);
+    const { client_name } = JSON.parse(event.body);
 
     const templatePath = path.join(__dirname, "contract-template.docx");
     const fileBuffer = fs.readFileSync(templatePath);
-
-    let content = fileBuffer.toString("binary");
-
-    content = content.replace(/{{client_name}}/g, client_name);
-    content = content.replace(/{{service_fee}}/g, service_fee);
-
-    const updatedBuffer = Buffer.from(content, "binary");
 
     return {
       statusCode: 200,
@@ -22,10 +15,10 @@ exports.handler = async (event) => {
           "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
         "Content-Disposition": `attachment; filename=Contract-${client_name}.docx`,
       },
-      body: updatedBuffer.toString("base64"),
+      body: fileBuffer.toString("base64"),
       isBase64Encoded: true,
     };
-  } catch (error) {
-    return { statusCode: 500, body: error.toString() };
+  } catch (err) {
+    return { statusCode: 500, body: err.toString() };
   }
 };
